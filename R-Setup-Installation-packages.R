d26e9a5dffbd3bb7ed08cb949095479437d56107
Rahul Madhavan <rahul.maddy@gmail.com>
2017-01-08 19:43:35 +0530

    Cleanup Job

--- R-Setup-Installation-packages.R
+++ R-Setup-Installation-packages.R
@@ -1,22 +1,30 @@
 # Packages to install on a new R setup
 
+
+check_and_install <- function(packagename){
+  if(!(packagename %in% installed.packages()[,"Package"])){
+    install.packages(packagename)
+  }
+}
+
+
 ####################
 # Packages to connect with DB
 ####################
 
-install.packages("RODBC") 
-install.packages("RMySQL")
-install.packages("RPostgresSQL")
-install.packages("RSQLite")
+check_and_install("RODBC") 
+check_and_install("RMySQL")
+check_and_install("RPostgreSQL")
+check_and_install("RSQLite")
 ####################
 # Packages to connect with Excel
 ####################
 
-install.packages("XLConnect")
-install.packages("xlsx")
+check_and_install("XLConnect")
+check_and_install("xlsx")
 
 # SASS
-install.packages("foreign")
+check_and_install("foreign")
 
 
 ####################
@@ -24,16 +32,16 @@ install.packages("foreign")
 ####################
 
 # dplyr - Essential shortcuts for subsetting, summarizing, rearranging, and joining together data sets. dplyr is our go to package for fast data manipulation.
-install.packages("dplyr")
+check_and_install("dplyr")
 
 # tidyr - Tools for changing the layout of your data sets. Use the gather and spread functions to convert your data into the tidy format, the layout R likes best.
-install.packages("dplyr")
+check_and_install("dplyr")
 
 # stringr - Easy to learn tools for regular expressions and character strings.
-install.packages("stringr")
+check_and_install("stringr")
 
 # lubridate - Tools that make working with dates and times easier.
-install.packages("lubridate")
+check_and_install("lubridate")
 
 
 ####################
@@ -41,39 +49,39 @@ install.packages("lubridate")
 ####################
 
 # ggplot2 - R's famous package for making beautiful graphics. ggplot2 lets you use the grammar of graphics to build layered, customizable plots.
-install.packages("ggplot2")
+check_and_install("ggplot2")
 
 # ggvis - Interactive, web based graphics built with the grammar of graphics.
-install.packages("ggvis")
+check_and_install("ggvis")
 
 # rgl - Interactive 3D visualizations with R
-install.packages("rgl")
+check_and_install("rgl")
 
 # htmlwidgets - A fast way to build interactive (javascript based) visualizations with R. Packages that implement htmlwidgets include:
-install.packages("htmlwidgets")
+check_and_install("htmlwidgets")
 
 
 # leaflet (maps)
-install.packages("leaflet")
+check_and_install("leaflet")
 
 # dygraphs (time series)
-install.packages("dygraphs")
+check_and_install("dygraphs")
 
 # DT (tables)
-install.packages("DT")
+check_and_install("DT")
 
 # diagrammeR (diagrams)
-install.packages("diagrammeR")
+check_and_install("DiagrammeR")
 
 # network3D (network graphs)
-install.packages("network3D")
+check_and_install("networkD3")
 
 # threeJS (3D scatterplots and globes).
-install.packages("threeJS")
+check_and_install("threejs")
 
 
 # googleVis - Let's you use Google Chart tools to visualize data in R. Google Chart tools used to be called Gapminder, the graphing software Hans Rosling made famous in hie TED talk.
-install.packages("googleVis")
+check_and_install("googleVis")
 
 
 ####################
@@ -81,35 +89,35 @@ install.packages("googleVis")
 ####################
 
 # car - car's Anova function is popular for making type II and type III Anova tables.
-install.packages("car")
+check_and_install("car")
 
 # mgcv - Generalized Additive Models
-install.packages("mgcv")
+check_and_install("mgcv")
 
 # lme4/nlme - Linear and Non-linear mixed effects models
-install.packages("lme4")
-install.packages("nlme")
+check_and_install("lme4")
+check_and_install("nlme")
 
 # randomForest - Random forest methods from machine learning
-install.packages("randomForest")
+check_and_install("randomForest")
 
 # multcomp - Tools for multiple comparison testing
-install.packages("multcomp")
+check_and_install("multcomp")
 
 # vcd - Visualization tools and tests for categorical data
-install.packages("vcd")
+check_and_install("vcd")
 
 
 # glmnet - Lasso and elastic-net regression methods with cross validation
-install.packages("glmnet")
+check_and_install("glmnet")
 
 
 # survival - Tools for survival analysis
-install.packages("survival")
+check_and_install("survival")
 
 
 # caret - Tools for training regression and classification models
-install.packages("caret")
+check_and_install("caret")
 
 
 
@@ -120,15 +128,15 @@ install.packages("caret")
 
 
 # shiny - Easily make interactive, web apps with R. A perfect way to explore data and share findings with non-programmers.
-install.packages("shiny")
+check_and_install("shiny")
 
 
 # R Markdown - The perfect workflow for reproducible reporting. Write R code in your markdown reports. When you run render, R Markdown will replace the code with its results and then export your report as an HTML, pdf, or MS Word document, or a HTML or pdf slideshow. The result? Automated reporting. R Markdown is integrated straight into RStudio.
-install.packages("rmarkdown")
+check_and_install("rmarkdown")
 
 
 # xtable - The xtable function takes an R object (like a data frame) and returns the latex or HTML code you need to paste a pretty version of the object into your documents. Copy and paste, or pair up with R Markdown.
-install.packages("xtable")
+check_and_install("xtable")
 
 
 ####################
@@ -136,16 +144,16 @@ install.packages("xtable")
 ####################
 
 # sp, maptools - Tools for loading and using spatial data including shapefiles.
-install.packages("sp")
-install.packages("maptools")
+check_and_install("sp")
+check_and_install("maptools")
 
 
 # maps - Easy to use map polygons for plots.
-install.packages("maps")
+check_and_install("maps")
 
 
 # ggmap - Download street maps straight from Google maps and use them as a background in your ggplots.
-install.packages("ggmap")
+check_and_install("ggmap")
 
 
 
@@ -154,41 +162,41 @@ install.packages("ggmap")
 ####################
 
 # zoo - Provides the most popular format for saving time series objects in R.
-install.packages("zoo")
+check_and_install("zoo")
 
 
 # xts - Very flexible tools for manipulating time series data sets.
-install.packages("xts")
+check_and_install("xts")
 
 
 # quantmod - Tools for downloading financial data, plotting common charts, and doing technical analysis.
-install.packages("quantmod")
+check_and_install("quantmod")
 
 ####################
 # To write high performance R code
 ####################
 
 # Rcpp - Write R functions that call C++ code for lightning fast speed.
-install.packages("Rcpp")
+check_and_install("Rcpp")
 
 # data.table - An alternative way to organize data sets for very, very fast operations. Useful for big data.
-install.packages("data.table")
+check_and_install("data.table")
 
 # parallel - Use parallel processing in R to speed up your code or to crunch large data sets.
-install.packages("parallel")
+check_and_install("parallel")
 
 ####################
 # To work with the web
 ####################
 
 # XML - Read and create XML documents with R
-install.packages("XML")
+check_and_install("XML")
 
 # jsonlite - Read and create JSON data tables with R
-install.packages("jsonlite")
+check_and_install("jsonlite")
 
 # httr - A set of useful tools for working with http connections
-install.packages("httr")
+check_and_install("httr")
 
 
 ####################
@@ -196,13 +204,13 @@ install.packages("httr")
 ####################
 
 # devtools - An essential suite of tools for turning your code into an R package.
-install.packages("devtools")
+check_and_install("devtools")
 
 # testthat - testthat provides an easy way to write unit tests for your code projects.
-install.packages("testthat")
+check_and_install("testthat")
 
 # roxygen2 - A quick way to document your R packages. roxygen2 turns inline code comments into documentation pages and builds a package namespace.
-install.packages("roxygen2")
+check_and_install("roxygen2")
 
 
 ####################################################################################################
@@ -214,81 +222,82 @@ install.packages("roxygen2")
 # PreModeling Stage
 ####################
 # Data Visualization
-install.packages("ggplot2")
-install.packages("googleVis")
+check_and_install("ggplot2")
+check_and_install("googleVis")
 
 # Data Transformation
-install.packages("plyr")
-install.packages("data.table")
+check_and_install("plyr")
+check_and_install("data.table")
 
 # Missing Values Imputation
-install.packages("MissForest")
-install.packages("MissMDA")
+check_and_install("missForest")
+check_and_install("missMDA")
 
 # Outliers Detection
-install.packages("Outliers")
-install.packages("EVIR")
+check_and_install("outliers")
+check_and_install("evir")
 
 
 # Feature Selection
-install.packages("Features")
-install.packages("RRF")
+check_and_install("features")
+check_and_install("RRF")
 
 
 # Dimension Reduction
-install.packages("FactoMiner")
-install.packages("CCP")
+check_and_install("FactoMineR")
+check_and_install("CCP")
 
 ####################
 # Modeling Stage
 ####################
 # continuous Regression
-install.packages("car")
-install.packages("randomforest")
+check_and_install("car")
+check_and_install("randomForest")
 
 # Ordinal Regression
-install.packages("RMiner")
-install.packages("CoreLearn")
+check_and_install("rminer")
+check_and_install("CORElearn")
 
 # Classfication
-install.packages("Caret")
-install.packages("BigRF")
+check_and_install("caret")
+# check_and_install("bigrf")
+# Removed from repository - use in case extremely necessary only
 
 # Clustering
-install.packages("CBA")
-install.packages("RankCluster")
+check_and_install("cba")
+check_and_install("Rankcluster")
 
 
 # TimeSeries
-install.packages("forecast")
-install.packages("LTSA")
+check_and_install("forecast")
+check_and_install("ltsa")
 
 # survival
-install.packages("survival")
-install.packages("Basta")
+check_and_install("survival")
+check_and_install("BaSTA")
 
 ####################
 # Post-Modeling Stage
 ####################
 # General Model Validation
-install.packages("LSMeans")
-install.packages("Comparison")
+check_and_install("lsmeans")
+check_and_install("comparison")
 
 # Regression Validation
-install.packages("RegTest")
-install.packages("ACD")
+check_and_install("regtest")
+check_and_install("ACD")
 
 # Classification Validation
-install.packages("BinomTools")
-install.packages("DAIM")
+check_and_install("binomTools")
+check_and_install("Daim")
 
 # Clustering Validation
-install.packages("ClustEval")
-install.packages("SigClust")
+check_and_install("clusteval")
+check_and_install("sigclust")
 
 # ROC Analysis
-install.packages("PROC")
-install.packages("TimeROC")
+check_and_install("pROC")
+check_and_install("timeROC")
 
 
 
@@ -311,63 +320,63 @@ install.packages("TimeROC")
 
 
 
-install.packages("car")
-install.packages("choroplethr")
-install.packages("data.table")
-install.packages("devtools")
-install.packages("diffobj")
-install.packages("dplyr")
-install.packages("DT")
-install.packages("dygraphs")
-install.packages("editR")
-install.packages("fitbitScraper")
-install.packages("flexdashboard")
-install.packages("foreach")
-install.packages("ggiraph")
-install.packages("ggplot2")
-install.packages("gmodels")
-install.packages("googlesheets")
-install.packages("googleVis")
-install.packages("httr")
-install.packages("installr")
-install.packages("janitor")
-install.packages("jsonlite")
-install.packages("knitr")
-install.packages("leaflet")
-install.packages("listviewer")
-install.packages("lubridate")
-install.packages("metricsgraphics")
-install.packages("MonetDBLite")
-install.packages("openxlsx")
-install.packages("plotly")
-install.packages("plyr")
-install.packages("profvis")
-install.packages("psych")
-install.packages("quantmod")
-install.packages("rcdimple")
-install.packages("RColorBrewer")
-install.packages("readr")
-install.packages("readxl")
-install.packages("reinstallr")
-install.packages("remotes")
-install.packages("reshape2")
-install.packages("rga")
-install.packages("rio")
+check_and_install("car")
+check_and_install("choroplethr")
+check_and_install("data.table")
+check_and_install("devtools")
+check_and_install("diffobj")
+check_and_install("dplyr")
+check_and_install("DT")
+check_and_install("dygraphs")
+
+check_and_install("fitbitScraper")
+check_and_install("flexdashboard")
+check_and_install("foreach")
+check_and_install("ggiraph")
+check_and_install("ggplot2")
+check_and_install("gmodels")
+check_and_install("googlesheets")
+check_and_install("googleVis")
+check_and_install("httr")
+check_and_install("installr")
+check_and_install("janitor")
+check_and_install("jsonlite")
+check_and_install("knitr")
+check_and_install("leaflet")
+check_and_install("listviewer")
+check_and_install("lubridate")
+check_and_install("metricsgraphics")
+check_and_install("MonetDBLite")
+check_and_install("openxlsx")
+check_and_install("plotly")
+check_and_install("plyr")
+check_and_install("profvis")
+check_and_install("psych")
+check_and_install("quantmod")
+devtools::install_github("timelyportfolio/rcdimple")
+check_and_install("RColorBrewer")
+check_and_install("readr")
+check_and_install("readxl")
+check_and_install("reinstallr")
+check_and_install("remotes")
+check_and_install("reshape2")
+check_and_install("RGA")
+check_and_install("rio")
 
 
-install.packages("RMySQL")
+check_and_install("RMySQL")
 
 
-install.packages("roxygen2")
-install.packages("RSiteCatalyst")
-install.packages("rvest")
-install.packages("scales")
-install.packages("shiny")
-install.packages("sqldf")
-install.packages("stringr")
-install.packages("tidyr")
-install.packages("tidytext")
-install.packages("tmap")
-install.packages("validate")
-install.packages("XML")
-install.packages("zoo")
+check_and_install("roxygen2")
+check_and_install("RSiteCatalyst")
+check_and_install("rvest")
+check_and_install("scales")
+check_and_install("shiny")
+check_and_install("sqldf")
+check_and_install("stringr")
+check_and_install("tidyr")
+check_and_install("tidytext")
+check_and_install("tmap")
+check_and_install("validate")
+check_and_install("XML")
+check_and_install("zoo")
